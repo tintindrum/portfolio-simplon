36d7985169142cc61beae9556462c76656782df0
tintindrum <quentin-drumel@hotmail.com>
2022-06-08 16:53:37 +0200

    maj structure landing page , ajout du template front-end et controller pour la partie back & front , style scss

--- src/Controller/HomeFrontController.php
+++ src/Controller/HomeFrontController.php
@@ -6,13 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HomeController extends AbstractController
+class HomeFrontController extends AbstractController
 {
-    #[Route('/accueil', name: 'app_main')]
+    #[Route('/accueil-front', name: 'app_mainfront')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+        return $this->render('homefront/index.html.twig', [
+            'controller_name' => 'HomeFrontController',
         ]);
     }
 }
--- src/Controller/LandingController.php
+++ src/Controller/LandingController.php
@@ -6,13 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HomeController extends AbstractController
+class LandingController extends AbstractController
 {
-    #[Route('/accueil', name: 'app_main')]
+    #[Route('/', name: 'app_landing')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+        return $this->render('landing/index.html.twig', [
+            'controller_name' => 'LandingController',
         ]);
     }
 }
