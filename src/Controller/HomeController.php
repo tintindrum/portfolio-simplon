e104efe0470567cbdaf6569fd78acf7d5cab0275
tintindrum <quentin-drumel@hotmail.com>
2022-06-09 19:38:43 +0200

    affichage projet de la bdd dans le home , creation des fichiers card pour caroussel & list grid

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,10 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/accueil', name: 'app_main')]
-    public function index(): Response
+    public function index(ArticleRepository $articleRepo): Response
     {
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'articles' => $articleRepo->findAll()
         ]);
     }
 }
